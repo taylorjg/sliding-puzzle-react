56a8bb1ad587b3ea8554817f6762439573bc1fbc
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-01-07 10:50:55 +0000

    Improve tile colours

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,5 @@
 import * as Phaser from "phaser"
-// import { makeTiles } from "./logic/logic"
+import { makeTiles } from "./logic/logic"
 import { range } from "./logic/utils"
 
 export interface PuzzleActions {
@@ -18,16 +18,15 @@ export const makePuzzleActions = (game: Phaser.Game): PuzzleActions => {
   }
 }
 
-class GameScene extends Phaser.Scene {
+class BoardScene extends Phaser.Scene {
 
   tiles: Phaser.GameObjects.Container[] = []
 
   public constructor() {
-    super("GameScene")
+    super("BoardScene")
   }
 
   public create() {
-    console.log("[GameScene#create]")
     this.game.events.on("ENTER_READONLY_MODE", this.onEnterReadonlyMode, this)
     this.game.events.on("RESET_BOARD", this.onResetBoard, this)
     this.game.events.on("START_SOLUTION_PRESENTATION", this.onStartSolutionPresentation, this)
@@ -62,20 +61,36 @@ class GameScene extends Phaser.Scene {
     const numCols = puzzle[0].length
 
     const GUTTER = 10
-    const { width } = this.sys.game.canvas
-    const tileSize = (width - (GUTTER * 2)) / numCols
+    const { width, height } = this.sys.game.canvas
+    const tileWidth = (width - (GUTTER * 2)) / numCols
+    const tileHeight = (height - (GUTTER * 2)) / numRows
+
+    const tileRefs = makeTiles(puzzle)
+
     for (const row of range(numRows)) {
       for (const col of range(numCols)) {
         const value = puzzle[row][col]
         if (value === 0) continue
-        const x = GUTTER + row * tileSize + tileSize / 2
-        const y = GUTTER + col * tileSize + tileSize / 2
-        const rectangle = this.add.rectangle(0, 0, tileSize, tileSize, 0xFFFF)
+        const x = GUTTER + col * tileWidth + tileWidth / 2
+        const y = GUTTER + row * tileHeight + tileHeight / 2
+
+        const goalValueTileRef = tileRefs.find(tileRef => tileRef.goalValue === value)
+        const [goalValueRow, goalValueCol] = goalValueTileRef?.position
+        const colour = (goalValueRow + goalValueCol) % 2
+          ? 0xFAEBD7 // AntiqueWhite
+          : 0xB22222 // Firebrick
+        const rectangle = this.add.rectangle(0, 0, tileWidth, tileHeight, colour)
         rectangle.setScale(0.98)
-        const text = this.add.text(0, 0, value.toString(), { fontSize: "48px" }).setOrigin(0.5)
+
+        const textStyle = {
+          fontSize: numRows === 4 ? "48px" : "64px",
+          color: "#FFD700" // Gold
+        }
+        const text = this.add.text(0, 0, value.toString(), textStyle).setOrigin(0.5)
+
         const tile = this.add.container(x, y, [rectangle, text])
         this.add.existing(tile)
-        tile.setSize(tileSize, tileSize)
+        tile.setSize(tileWidth, tileHeight)
         tile.setInteractive({ useHandCursor: true })
         tile.on(Phaser.Input.Events.POINTER_DOWN, this.onTileClick, this)
         this.tiles.push(tile)
@@ -97,7 +112,7 @@ const gameConfig: Phaser.Types.Core.GameConfig = {
     mode: Phaser.Scale.FIT
   },
   backgroundColor: "#000000",
-  scene: [GameScene],
+  scene: [BoardScene],
   parent: "puzzle"
 }
 
